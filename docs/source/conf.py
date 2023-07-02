d087b6766bb56fa5f1d069030d8b98e48f622f7c
Ragnhild Sundsbak <ragnhild.sundsbak@ub.uio.no>
2023-07-02 14:50:16 +0200

    Update conf.py
    html_theme = "default"
        sidebarbgcolor = "DarkOrange"

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,9 +31,10 @@ templates_path = ['_templates']
 
 # html_theme = 'sphinx_rtd_theme'
 
-# html_theme = "default"
+html_theme = "default"
+    sidebarbgcolor = "DarkOrange"
 
-html_theme = "pyramid"
+# html_theme = "pyramid"
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'

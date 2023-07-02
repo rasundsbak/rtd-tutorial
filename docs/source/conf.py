c4716fd5e80a9e119e5428cde453190462450aee
Ragnhild Sundsbak <ragnhild.sundsbak@ub.uio.no>
2023-07-02 15:21:41 +0200

    Update conf.py
    html_theme = 'default'
    sidebarbgcolor = '#FF8C00'

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,8 +31,8 @@ templates_path = ['_templates']
 
 # html_theme = 'sphinx_rtd_theme'
 
-html_theme = "default"
-sidebarbgcolor = "DarkOrange"
+html_theme = 'default'
+sidebarbgcolor = '#FF8C00'
 
 # html_theme = "pyramid"
 

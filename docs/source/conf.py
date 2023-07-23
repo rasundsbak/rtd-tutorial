fce6ff2a0527a198180641ac231b4d5e88f47b31
Ragnhild Sundsbak <ragnhild.sundsbak@ub.uio.no>
2023-07-23 16:59:48 +0200

    Update conf.py
    error msg: from rtd: Running Sphinx v5.3.0
    making output directory... done
    WARNING: while setting up extension conf.py: The app.add_stylesheet() is deprecated. Please use app.add_css_file() instead.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,8 +45,8 @@ html_static_path = ['_static']
 # def setup(app):
 #    app.add_css_file("custom.css")
 
-def setup(app):     
-    app.add_stylesheet('custom.css')
+def setup(app):
+    app.add_css_file('custom.css')
     app.add_javascript("custom.js")
     app.add_javascript("https://cdn.jsdelivr.net/npm/clipboard@1/dist/clipboard.min.js")
 

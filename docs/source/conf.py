c019fe055abed5258856cd66b3591701e3ec9e93
Ragnhild Sundsbak <ragnhild.sundsbak@ub.uio.no>
2023-07-23 23:45:28 +0200

    Update conf.py
    messing w py file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -65,16 +65,22 @@ def setup(app):
 sidebarbgcolor = 'DarkOrange'
 
 # html_theme = 'sphinx_rtd_theme'
-
+html_theme = "sphinx_book_theme"
 # default works, but not to change colour
 # html_theme = 'default'
 # sidebarbgcolor = '#FF8C00'
 # sidebarbgcolor = 'DarkOrange'
-
 # html_theme = "pyramid"
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
+html_logo = "../sphinx_copybutton/_static/copy-button.svg"
+html_title = "Sphinx Copybutton"
 
 # changing layout: https://sphinx-rtd-trial.readthedocs.io/en/1.1.3/theming.html
 # css colours: https://www.w3schools.com/cssref/css_colors.php

f4defe9d129be8ba00972d579ae2f0f44b987555
Ragnhild Sundsbak <ragnhild.sundsbak@ub.uio.no>
2023-07-23 19:51:49 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,6 +46,7 @@ html_static_path = ['_static']
 # def setup(app):
 #    app.add_css_file("custom.css")
 
+```python
 def setup(app):
     app.add_css_file('custom.css')
     app.add_javascript("custom.js")

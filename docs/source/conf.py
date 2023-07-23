85736613112f9121507448e7d46e4a8ec2f117fe
Ragnhild Sundsbak <ragnhild.sundsbak@ub.uio.no>
2023-07-23 16:34:57 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,7 +41,6 @@ html_static_path = ['_static']
 
 # from: https://stackoverflow.com/questions/44793811/change-the-colors-of-the-sphinx-read-the-docs-theme
 def setup(app):
-    app.add_css_file('custom.css')
     app.add_stylesheet('custom.css')
     app.add_javascript("custom.js")
     app.add_javascript("https://cdn.jsdelivr.net/npm/clipboard@1/dist/clipboard.min.js")

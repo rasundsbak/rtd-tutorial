2a4d73a31a7db7dd7a75953d9b1ffa5bfcd3cae1
Ragnhild Sundsbak <ragnhild.sundsbak@ub.uio.no>
2023-09-25 16:37:46 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Unofficial ComtradR tutorial, download data with R'
+copyright = '2023, Ragnhild'
+author = 'Ragnhild'
 
 release = '0.1'
 version = '0.1.0'
